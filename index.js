d703775cfee1a06b9cae2528dcbcd77a76da1b00
John Smart <smartj@gmail.com>
2012-12-14 15:12:36 -0800

    adding bi-directional streams

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
 	ConsumerStream: require('./lib/consumer_stream'),
-	ProducerStream: require('./lib/producer_stream')
+	ProducerStream: require('./lib/producer_stream'),
+	BiStream: require('./lib/bi_stream')
 };
--- test/test_streams.js
+++ test/test_streams.js
@@ -46,4 +46,44 @@ describe('Produce and Consumer', function() {
 
 		p.destroy();
 	});
+
+	it('three stages', function(done) {
+		var p = new streams.ProducerStream();
+		var b = new streams.BiStream();
+		var c = new streams.ConsumerStream();
+
+		var pData = false;
+		p.on('data', function() {
+			pData = true;
+		});
+
+		var bData = false;
+		b.on('data', function() {
+			assert.ok(pData, 'producer must first produce data');
+			bData = true;
+		});
+
+		p.pipe(b);
+		b.pipe(c);
+
+		var cDrain = false;
+		c.on('drain', function() {
+			cDrain = true;
+			assert.equal(1, p.countDownstream);
+			assert.equal(1, c.countUpstream);
+			assert.equal(1, b.countDownstream);
+			assert.equal(1, b.countUpstream);
+		});
+
+		c.on('close', function() {
+			assert.ok(cDrain, 'consumer never drained before closing');
+			assert.ok(pData, 'producer did not produce data');
+			assert.ok(bData, 'bi-directional did not produce data');
+			assert.equal(1, b.countProcessed);
+			done();
+		});
+
+		p._produceData('hello world');
+		p.destroy();
+	});
 });
